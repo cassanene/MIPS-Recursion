73703e032e940e4e0bd0f4f0eb52442fbb13ab01
Cassey Anene <casseyanene@gmail.com>
2020-03-31 18:48:51 -0500

    Update recursion.s
    reconfigured a lot of broken syntax

--- recursion.s
+++ recursion.s
@@ -1,11 +1,13 @@
 .data
+# BASE = 34
+# RANGE 24 - X
     prompt: .asciiz "Input: "
     validString: .space 21
     userInput: .space 101
     invalidInput: .asciiz "Invalid input"
     testString: .asciiz "Test"
 .text
-
+.globl main
 main:
 #prompt the user for a input
     li $v0, 4
@@ -15,41 +17,36 @@ main:
 #store the characters inputted by the users
     li $v0, 8 #systemcalls
     la $a0, userInput
+    li $a1, 101
     syscall
-    li $t0, 32
-    li $s0, 0 #counter
 
-    li $s2, 0 #for the number of char its been through
-    la $t1, userInput 
-    li $s3, 0 #tracks total num of valid chars
-    li $s4, 0 #counter for the number of spaces before the input
-    lb $s1, 0($a1)
-    # li $s3, 0 #index of the valid string
+    li $s0, 0 #counter for the length of the valid string
+    li $s1, 0 #counter for the length before 
 
 removeleadandtrail:
-    # beq $s1, 32, skip #if the character is a space it will go to this label
-    # beq $s1, 9, skip
-    beq $s1, 49, makenewstring
-    # ble $s1, 57, makenewstring
-    # bge $s1, 88, makenewstring #goes to X
+    lb $t2, 0($a0)
+    beq $t2, 10, print #print after the string is done
+    beq $t2, 32, skip #if the character is a space it will go to this label
+    beq $t2, 9, skip
+    ble $s1, 57, countValid
+    # bge $s1, 88, countValid #goes to X
     # ble $s1, 120, makenewstrng
-    # beq $s1, 10, print
 
-# skip:
-#     bge $s2, 1, invalid
 
-# j removeleadandtrail
+skip:
+    addi $s1, $s1, 1 #adding 1 to the number of tabs and spaces before
+    bge $s0, 1, invalid #counter for the length of the 
 
-# invalid:
-#     li $v0, 4
-#     la $a0, invalidInput
-#     syscall
+j removeleadandtrail
 
-makenewstring:
-    # sw $s1, validString($s3)
-    addi $s2,$s2, 1 #moving the index
-    addi $s1, $s1, 1 
-    addi $s3, $s3, 1
+invalid:
+    li $v0, 4
+    la $a0, invalidInput
+    syscall
+
+countValid:
+    addi $s0,$s0, 1
+    addi $a0, $a0, 1 #move index
 
     li $v0, 4
     la $a0, testString
